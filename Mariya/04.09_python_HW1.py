46d75e19d31c68317c849bbdd164eedbf9795d81
violetpurplepalm <violetpurplepalm@gmail.com>
2022-09-01 17:43:22 +0300

    Add 04.09_python_HW1.py

---
+++ Mariya/04.09_python_HW1.py
@@ -0,0 +1,15 @@
+
+# 1
+print("Hello world!")
+
+# 2
+print("What's your name?")
+user_name = input()
+print(f'Hello {user_name}!')
+
+# 3
+print('*' * 9)
+print('*', ' '*5, '*')
+print('*', ' '*5, '*')
+print('*' * 9)
+
